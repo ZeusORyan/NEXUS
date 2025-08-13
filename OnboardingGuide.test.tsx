1df2eede19bd1e4a90092a57188bfeebb25608e4
Joshua Matney <joshuarmatney@gmail.com>
2025-08-13 11:50:34 -0400

    Fix OnboardingGuide test and update package files

--- OnboardingGuide.test.tsx
+++ OnboardingGuide.test.tsx
@@ -1,4 +1,12 @@
+// jest.config.js
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+};
+
+// src/setupTests.js
 import '@testing-library/jest-dom';
+
+// src/OnboardingGuide.test.tsx
 import { render, screen, fireEvent } from '@testing-library/react';
 import OnboardingGuide from './OnboardingGuide';
 
